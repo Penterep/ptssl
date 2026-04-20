18541cf2a3be8e44feb21561a0e4cb9708953d7d
KakonRaketak <repko.matus@gmail.com>
2026-04-20 20:00:59 +0200

    Task PT-1102 and 1101 -fix module order -fix protocols problem

--- ptssl/ptssl.py
+++ ptssl/ptssl.py
@@ -31,7 +31,6 @@ import fcntl
 import uuid
 import requests
 
-
 from io import StringIO
 from types import ModuleType
 from urllib.parse import urlparse, urlunparse
@@ -48,6 +47,8 @@ from helpers.helpers import Helpers
 from _version import __version__
 
 STARTTLS_PROTOCOLS = ["ftp", "smtp", "lmtp", "pop3", "imap", "xmpp", "xmpp-server", "telnet", "ldap", "nntp", "sieve", "postgres", "mysql"]
+
+MODULE_ORDER = ["tsd", "alt", "pt", "alg", "sa", "pct", "ct", "fst", "gt", "bvt", "httpr", "hsts"]
 
 
 class PtSSL:
@@ -66,8 +67,16 @@ class PtSSL:
 
     def run(self) -> None:
         """Main method"""
-        tests = self.args.tests or _get_all_available_modules()
-        self.ptthreads.threads(tests, self.run_single_module, self.args.threads)
+        tests = self.args.tests or [m for m in MODULE_ORDER if m in _get_all_available_modules()]
+        self._module_outputs = {}
+        self.ptthreads.threads(list(tests), self.run_single_module, self.args.threads)
+
+        # Print module outputs in the defined order
+        for module_name in tests:
+            output = self._module_outputs.get(module_name, "")
+            if output:
+                sys.stdout.write(output)
+                sys.stdout.flush()
 
         self.ptjsonlib.set_status("finished")
         ptprint(self.ptjsonlib.get_result_json(), "", self.args.json)
@@ -288,13 +297,10 @@ class PtSSL:
 
                 except Exception as e:
                     ptprint(e, "ERROR", not self.args.json)
-                    error = e
-                else:
-                    error = None
                 finally:
                     self.thread_local_stdout.clear_thread_buffer()
                     with self._lock:
-                        ptprint(buffer.getvalue(), "TEXT", not self.args.json, end="\n")
+                        self._module_outputs[module_name] = buffer.getvalue()
             else:
                 ptprint(f"Module '{module_name}' does not have 'run' function", "WARNING", not self.args.json)
 
